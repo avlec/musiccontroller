9ea9e8e79f81ad79820f9fba1815b577f657ffd0
Alec Cox <avlec@protonmail.com>
2018-06-01 11:02:17 -0700

    First push.

---
+++ popup.js
@@ -0,0 +1,19 @@
+alert("AAAA");
+/*
+
+chrome.tabs.query({ url: "https://play.google.com/music/listen*" }, function(result) {
+    if(result.length === 0) {
+        alert("No open music tabs.");
+        return;
+    }
+    alert("Tab selection being made from " + result.length + " tab(s).");
+    if(result.length !== 0) {
+        // Some display here prompting which window
+        selected_tab_id = result[0].id;
+    } else {
+        selected_tab_id = result[0].id;
+    }
+    alert(selected_tab_name + " id: " + selected_tab_id);
+});
+
+*/
