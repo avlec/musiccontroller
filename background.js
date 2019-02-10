fd24043393a9702fd09a4e1ac6ed481becf4b9bd
London Drugs <londondrugs@londons-imac.lan>
2019-02-10 11:43:37 -0800

    generalized background.js

--- background.js
+++ background.js
@@ -1,28 +1,42 @@
+/**
+ * tab: chrome tab object
+ * jseventhandle: javascript file that is run on media key action
+ */
+var chosen_source = {
+  tab: null,
+  jseventhandle: ''
+}
 
-var selected_tab_id;
-var selected_tab_name = "Google Play Music";
+/**
+ * list of dict's for sources supported by the extension
+ * {
+ * url: matching url string during parsing
+ * jseventhandle: javascript file for handling the prev/next/play/pause
+ * potential_sources: all open tabs matching url
+ * }
+ */
+var supported_sources = [
+  { url: 'https://play.google.com/music/listen*', jseventhandle: 'googleplayclick.js', potential_sources: []},
+  { url: 'https://music.youtube.com/*', jseventhandle: '', potential_sources: []}
+]
 
+/* Enumerate all the supported sources */
+supported_sources.forEach(function (supported_source) {
+  chrome.tabs.query({ url: supported_source.url }, function (tabs) {
+    result.forEach(function (result) {
+      supported_source.potential_sources.push(result)
+    })
+  })
+})
 
-/* this script selects the play music tab */
-chrome.tabs.query({ url: "https://play.google.com/music/listen*" }, function(result) {
-  if(result.length === 0) {
-    alert("No open music tabs.");
-    return;
-  }
-  alert("Tab selection being made from " + result.length + " tab(s).");
-  if(result.length !== 0) {
-    // Some display here prompting which window
-    selected_tab_id = result[0].id;
-  } else {
-    selected_tab_id = result[0].id;
-  }
-  alert(selected_tab_name + " id: " + selected_tab_id);
-});
+/* Present users with source options. */
+// TODO define this page and how it functions
+chrome.tabs.create({ url: '', active: true }, function (tab) { return })
 
 chrome.commands.onCommand.addListener(function (command) {
-  chrome.tabs.executeScript(selected_tab_id, {
+  chrome.tabs.executeScript(chosen_source.tab.id, {
     code: 'var button_type = "' + command + '"'
-  }, function() {
-    chrome.tabs.executeScript(selected_tab_id, { file: "simulate_click.js" });
-  });
-});
+  }, function () {
+    chrome.tabs.executeScript(chosen_source.tab.id, { file: chosen_source.jseventhandle })
+  })
+})
