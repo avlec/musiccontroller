0e1a35169f79b45bcd6bfc15fda8b240d0198400
London Drugs <londondrugs@londons-imac.lan>
2019-02-10 17:32:41 -0800

    saving remote

--- src/mediacontrollercallback.js
+++ src/mediacontrollercallback.js
@@ -2,7 +2,14 @@
 
 var media_source
 
+// Fetch state.
 chrome.storage.sync.get([ 'media_source' ], function(result){
     media_source = (Array.isArray() ? result[0] : result)
     console.log(media_source)
+})
+
+// Execute appropriate script ( media_source.jseventhandle )
+chrome.tabs.executeScript(media_source.tab.id, {
+    code:   'var eventinfo = ' + String({ 'command': command, playing: media_state.playing}),
+    file:   '/src/' + media_source.jseventhandle
 })
